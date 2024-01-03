0eba8731dc4780f839644fe0204ee67f77e765c3
AelinGalathyn <szandra.szabina9@gmail.com>
2024-01-03 17:28:26 +0100

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KonyvtarAsztaliWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Statisztika statisztika;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Beolvasas();
+        }
+
+        private void Beolvasas()
+        {
+            statisztika = new Statisztika();
+            dataGrid.ItemsSource = statisztika.Konyvek;
+        }
+
+        private void btnTorles_Click(object sender, RoutedEventArgs e)
+        {
+            Konyv konyv = dataGrid.SelectedItem as Konyv;
+            if(konyv != null)
+            {
+                MessageBoxResult result = MessageBox.Show("Biztos szeretné törölni a kiválasztott könyvet?", "Megerősítés", MessageBoxButton.YesNo);
+                if(result == MessageBoxResult.Yes)
+                {
+                    if (statisztika.Torles(konyv))
+                    {
+                        statisztika.Konyvek.Remove(konyv);
+                        MessageBox.Show("Sikeres törlés!");
+                        dataGrid.Items.Refresh();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Sikertelen törlés.");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Törléshez előbb válasszon ki könyvet!");
+            }
+        }
+    }
+}
---
+++ Statisztika.cs
@@ -0,0 +1,78 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace KonyvtarAsztaliWPF
+{
+    public class Statisztika
+    {
+        private List<Konyv> konyvek;
+        private MySqlConnectionStringBuilder connectionStringBuilder;
+
+        public Statisztika()
+        {
+            konyvek = new List<Konyv>();
+            connectionStringBuilder = new MySqlConnectionStringBuilder();
+            Beolvas();
+        }
+
+        private void Beolvas()
+        {
+            connectionStringBuilder.Server = "localhost";
+            connectionStringBuilder.Port = 3306;
+            connectionStringBuilder.Database = "vizsga-2022-14s-wip-db";
+            connectionStringBuilder.UserID = "root";
+            connectionStringBuilder.Password = "";
+
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionStringBuilder.ConnectionString))
+                {
+                    connection.Open();
+                    string sql = "SELECT * FROM books";
+                    MySqlCommand command = connection.CreateCommand();
+                    command.CommandText = sql;
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            konyvek.Add(new Konyv(reader.GetInt32("id"), reader.GetString("title"), reader.GetString("author"), reader.GetInt32("publish_year"), reader.GetInt32("page_count")));
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("An error occurred. The application will now close.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                Application.Current.Shutdown();
+            }
+        }
+
+        public bool Torles(Konyv konyv)
+        {
+            bool isDeleted = false;
+
+            using (MySqlConnection connection = new MySqlConnection(connectionStringBuilder.ConnectionString))
+            {
+                connection.Open();
+                string sql = $"DELETE FROM books WHERE id = {konyv.Id}";
+                MySqlCommand command = connection.CreateCommand();
+                command.CommandText = sql;
+
+                int rowsAffected = command.ExecuteNonQuery();
+
+                if (rowsAffected > 0)
+                {
+                    isDeleted = true;
+                }
+            }
+            return isDeleted;
+        }
+
+        public List<Konyv> Konyvek { get => konyvek; set => konyvek = value; }
+    }
+}
